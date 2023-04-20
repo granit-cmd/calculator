2c486614b634998bae1c16d221a440f1c77f5460
granit-cmd <P@ssw0rd13!#>
2023-04-20 16:20:32 +0600

    Init

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int res = 0;
+        int a = Character.getNumericValue(str.charAt(0));
+        int b = Character.getNumericValue(str.charAt(2));
+        char operation = str.charAt(1);
+        if (str.length() == 3 && (0<a&b<11)) {
+
+
+
+            switch (operation) {
+                case '+':
+                     res = a+b;
+                    System.out.println(res);
+                    break;
+                case '-':
+                    res = a-b;
+                    break;
+                case '*':
+                    res = a*b;
+                    break;
+                case '/':
+                        res = a/b;
+                    break;
+            }
+
+        } else {
+
+            System.out.println("Error something went wrong, Вам необходимо вводить числа с 1 до 10 в формате a+b ");
+        }
+        System.out.println(res);
+
+
+    }
+}
